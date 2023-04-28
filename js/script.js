3f3a390d047f2bfcb66fada7eb7421f91a889cb2
lucaassp <lucaspereira.student@gmail.com>
2023-04-28 17:06:49 -0300

    Fetch async

--- js/script.js
+++ js/script.js
@@ -1,49 +1,73 @@
-window.addEventListener('load', () => {
+const searchForm = document.getElementById('search-form-id');
+const searchInput = document.getElementById('search-input');
+const heroInfo = document.getElementById('hero-info');
+const heroThumbnail = document.getElementById('content-thumbnail');
+
+window.addEventListener('load', async () => {
+  
+    searchInput.addEventListener('keyup', async (e) => {
+        e.preventDefault();
+        if (e === 'Enter') {
+            await fetchMarvelCharacters(); 
+        }
+    })
+
+    searchForm.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        await fetchMarvelCharacters();
+    });
     
-    const timeStamp = '1';
-    const apiKey = 'd28ef029a68ad984b5e2be6fa65b6512';
-    const md5 = '051c2c5faf0162b6e3b25682a3af5a58';
 
-    const searchForm = document.getElementById('search-form-id');
-    const searchInput = document.getElementById('search-input');
-    const heroList = document.getElementById('hero-list');
-    const heroThumbnail = document.getElementById('content-thumbnail');
-
-    searchForm.addEventListener('submit', event => {
-        event.preventDefault();
+    async function fetchMarvelCharacters() {
+        const timeStamp = '1';
+        const apiKey = 'd28ef029a68ad984b5e2be6fa65b6512';
+        const md5 = '051c2c5faf0162b6e3b25682a3af5a58';
         const searchTerm = searchInput.value;
         const url = `https://gateway.marvel.com/v1/public/characters?nameStartsWith=${searchTerm}&ts=${timeStamp}&apikey=${apiKey}&hash=${md5}`;
 
-        console.log(url)
+        const response = await fetch(url);
+        const data = await response.json();
 
-        fetch(url).then(response => response.json()).then(data => {
-            const results = data.data.results;
-            heroList.innerHTML = '';
-            heroThumbnail.innerHTML = '';
-            results.forEach(hero => {
-                const heroDiv = document.createElement('ul');
-                heroDiv.classList.add('hero');
-                const heroName = document.createElement('li');
-                heroName.textContent = hero.name;
-                heroName.addEventListener('mouseover', () => {
-                    if (hero.thumbnail) {
-                        heroThumbnail.innerHTML = `<img src="${hero.thumbnail.path}.${hero.thumbnail.extension}">`;
-                    }
-                });
-                heroDiv.appendChild(heroName);
-                heroList.appendChild(heroDiv);
+        const { results: heroes } = data.data;
 
-                // Adiciona a thumbnail correspondente na div ao carregar a página
-                if (hero.thumbnail) {
-                    heroThumbnail.innerHTML = `<img src="${hero.thumbnail.path}.${hero.thumbnail.extension}">`;
-                }
-            });
+        const heroData = heroes.map(({ name, description, thumbnail, comics, series, stories }) => ({
+            name,
+            description,
+            thumbnail,
+            comics,
+            series,
+            stories,
+        }));
+
+        displayHeroData(heroData);
+    }
+
+    function displayHeroData(heroData) {
+    heroInfo.innerHTML = '';
+
+    heroData.forEach((hero) => {
+        const heroEl = document.createElement('div');
+        heroEl.classList.add('hero');
+        heroEl.innerHTML =
+
+        `<div class="hero-details">
+            <h3 class="hero-name">${hero.name}</h3>
+            <p class="hero-description">${hero.description || 'No description available.'}</p>
+        </div>
+        `;
+        heroThumbnail.innerHTML = '';
+
+        heroInfo.appendChild(heroEl);
+        if (hero.thumbnail) {
+            heroThumbnail.innerHTML = `<img src="${hero.thumbnail.path}.${hero.thumbnail.extension}">`;
+        }
+    });
+    }
+
+    function thumbnail(thumbnail) {
+        thumbnail.forEach(t => {
+
         })
-        .catch(error => {
-            console.error(error);
-            heroList.innerHTML = '<p>Failed to load heroes. Please try again later.</p>';
-        });
+    }
+});
 
-    })
-
-})
