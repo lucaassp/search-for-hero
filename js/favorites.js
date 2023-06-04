6d2f9d806e65fd0f1ef311473ae0a101c050e2e0
lucaassp <lucaspereira.student@gmail.com>
2023-06-04 18:53:37 -0300

    add funcionalidade de remover favoritos

--- js/favorites.js
+++ js/favorites.js
@@ -5,20 +5,38 @@ window.addEventListener('load', () => {
     favorites.forEach((hero) => {
       const heroCard = document.createElement('div');
       heroCard.classList.add('hero-card');
+      heroCard.id = `hero-${hero.id}`;
   
       const heroImage = document.createElement('img');
       heroImage.src = `${hero.thumbnail.path}.${hero.thumbnail.extension}`;
       heroImage.alt = hero.name;
       heroImage.classList.add('hero-image');
   
-      const heroName = document.createElement('h2');
+      const heroName = document.createElement('p');
       heroName.textContent = hero.name;
       heroName.classList.add('hero-name');
+  
+      const removeIcon = document.createElement('i');
+      removeIcon.classList.add('fa-solid', 'fa-minus');
+      removeIcon.style.marginLeft = '5px';
+      removeIcon.addEventListener('click', () => {
+        removeFavorite(hero);
+      });
+  
+      heroName.appendChild(removeIcon);
   
       heroCard.appendChild(heroImage);
       heroCard.appendChild(heroName);
   
       favoritesContainer.appendChild(heroCard);
     });
+  
+    function removeFavorite(hero) {
+        const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+        const updatedFavorites = favorites.filter((favorite) => favorite.id !== hero.id);
+        localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
+        const heroCard = document.getElementById(`hero-${hero.id}`);
+        heroCard.remove();
+    }
   });
   
